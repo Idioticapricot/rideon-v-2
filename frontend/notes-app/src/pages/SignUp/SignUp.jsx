a732e0e1c20e2c34ffc4da5f6b10a4f7e382379e
Idioticapricot <akarthikeyasmail@gmail.com>
2025-03-22 13:36:15 +0530

    Sign-up

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import PasswordInput from '../Login/PasswordInput';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-  return (
-    <div>
-      Signup
-      
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default SignUp
+  const validateEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Name is required");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Invalid Email");
+      return;
+    }
+
+    if (!password) {
+      setError("Password is required");
+      return;
+    }
+
+    setError(""); // Clear error if all validations pass
+
+    // Proceed with sign-up logic
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h1 className="text-2xl mb-7">SignUp</h1>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+            <button type="submit" className="btn-primary">
+              SignUp
+            </button>
+            <p className="text-sm text-center mt-4">
+              Already registered?
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
